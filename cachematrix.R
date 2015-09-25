ea81b144aa37249fc82a7ad7b81405e8353c4b53
adamsja7 <adams.jonathana@gmail.com>
2015-09-25 11:03:40 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The code will return a list with functions to 
+##      --set a matrix
+##      --get the matrix
+##      --set the inverse of the matrix
+##      --get the inverse of the matrix
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This will create a function called makeCacheMatrix()
 
+makeCacheMatrix <- function(f = matrix()) {
+##      --f is created as a square matrix that can be invertable
+
+        inv = NULL
+        
+        set = finction(u) {
+        
+                f <<- u
+                inv <<- NULL
+##      --'<<-' is used to assign the values of these objects which are not in the current working environment
 }
 
+        get = function() f
+        setinv = function(inverse) inv <<- inverse
+        getinv = function() inv
+        list(set=set, get=get, setinv=setinv, getinv=getinv)
+##      -- will return a list whichi will 
+##      --set a matrix
+##      --get the matrix
+##      --set the inverse of the matrix
+##      --get the inverse of the matrix
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##      --f will be the output of the makeCacheMatrix() function
+
+
+cacheSolve <- function(f, ...) {
+        ## Return a matrix that is the inverse of 'f'
+        inv =  f$getinv()
+  
+  
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  ##  if the inverse has already been calculated it will return a message
+  ##  stating it is getting the inverse from the cache
+  ##  it will skip the computation
+    
+  }
+  
+  mat.data = f$get()
+  inv = solve(mat.data, ...)
+  ##  If the inverse has not been calculated, it will calculate the inverse
+  
+  f$setinv(inv)
+  ##  sets the value of the inverese in teh cache from the setinv function
+  
+  return(inv)
+  
+  ## returns the inverse of the original matrix input to the makeCacheMatrix function
+  
+
 }
